0ea8de8880c65b6b8169355d29f8c3bf87db9b2f
ernane623 <85587472+ernane623@users.noreply.github.com>
2024-02-29 21:08:41 -0300

    meu primeiro commit

---
+++ src/components/Multi.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text } from 'react-native'
+import estilo from './Estilo'
+
+function Comp() {
+    return <Text>Comp #Oficial</Text>
+}
+
+function Comp1() {
+    return <Text>Comp #O1</Text>
+}
+
+function Comp2() {
+    return <Text>Comp #O2</Text>
+}
+
+export {Comp1, Comp2}
+export default Comp
